e22a6fa80a9e8543b5b68764537d9742829c99e9
wxweven <wxweven@qq.com>
2021-03-05 21:46:47 +0800

    add angel

---
+++ src/main/java/com/wxweven/algorithm/Angel.java
@@ -0,0 +1,21 @@
+/**
+ * @(#)Angel.java, 2月 17, 2021.
+ * <p>
+ * Copyright 2021 fenbi.com. All rights reserved.
+ * FENBI.COM PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com.wxweven.algorithm;
+
+/**
+ * @author wxweven
+ */
+public class Angel {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Hello World, 王佳柠");
+
+
+    }
+}
